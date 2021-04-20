9b358780831ad4c10b6eeb0e52d774d4eafe84df
Rwan <rwan.souza@hotmail.com>
2021-04-20 17:19:52 -0300

    Dashboard Styled

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/github-icon.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github" />
+      <Title>Explore repositórios no GitHub</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/58310289?v=4"
+            alt="Rwan Souza"
+          />
+
+          <div>
+            <strong>rockeatseat/unform</strong>
+            <p>easy peasy highly scalable ReactJS & React Native forms</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/58310289?v=4"
+            alt="Rwan Souza"
+          />
+
+          <div>
+            <strong>rockeatseat/unform</strong>
+            <p>easy peasy highly scalable ReactJS & React Native forms</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/58310289?v=4"
+            alt="Rwan Souza"
+          />
+
+          <div>
+            <strong>rockeatseat/unform</strong>
+            <p>easy peasy highly scalable ReactJS & React Native forms</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 export default Dashboard;
