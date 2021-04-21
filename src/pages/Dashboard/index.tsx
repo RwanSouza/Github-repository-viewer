61cdca2b4050ae88cc229e581d292eaf94bc77f5
Rwan <rwan.souza@hotmail.com>
2021-04-21 19:46:45 -0300

    Styles repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api';
 
 import logoImg from '../../assets/github-icon.svg';
@@ -53,7 +55,7 @@ const Dashboard: React.FC = () => {
 
       setRepositories([...repositories, repository]);
       setNewRepo('');
-      setInputError(' ');
+      setInputError('');
     } catch (err) {
       setInputError('Error na busca de repositório');
     }
@@ -76,7 +78,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -88,7 +93,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,65 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/github-icon.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/2254731?v=4"
+            alt="face"
+          />
+          <div>
+            <strong>Facebook react</strong>
+            <p>descrição do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>100</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>100</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>100</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>asd</strong>
+            <p>sad</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 export default Repository;
